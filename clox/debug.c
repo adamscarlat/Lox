66db5015fb33111ad202a46cb9fdc0a2da7800d8
adamscarlat <ascarlat@vividcloud.com>
2021-12-26 11:49:35 -0500

    classes and fields

--- clox/debug.c
+++ clox/debug.c
@@ -73,7 +73,11 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     case OP_GET_UPVALUE:
       return byteInstruction("OP_GET_UPVALUE", chunk, offset);
     case OP_SET_UPVALUE:
-      return byteInstruction("OP_SET_UPVALUE", chunk, offset);                       
+      return byteInstruction("OP_SET_UPVALUE", chunk, offset);
+    case OP_GET_PROPERTY:
+      return constantInstruction("OP_GET_PROPERTY", chunk, offset);
+    case OP_SET_PROPERTY:
+      return constantInstruction("OP_SET_PROPERTY", chunk, offset);                             
     case OP_EQUAL:
       return simpleInstruction("OP_EQUAL", offset);
     case OP_GREATER:
@@ -121,7 +125,9 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
     case OP_LOOP:
-      return jumpInstruction("OP_LOOP", -1, chunk, offset);      
+      return jumpInstruction("OP_LOOP", -1, chunk, offset);
+    case OP_CLASS:
+      return constantInstruction("OP_CLASS", chunk, offset);            
     default:
       printf("Unknown opcode %d\n", instruction);
       return offset + 1;
